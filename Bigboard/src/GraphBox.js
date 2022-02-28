2c96027c61cd34d32c9abd06b22cd883b63ce089
STpdx <email-address>
2022-02-28 12:19:56 -0800

    added code in app.css and some changes

--- Bigboard/src/GraphBox.js
+++ Bigboard/src/GraphBox.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import chart from 'chart.js';
-class GraphBox extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>Graph</h2>
-
-            </div>
-
-        );
-    };
+import React from "react";
+import chart from "chart.js";
+class GraphBox extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Graph</h2>
+      </div>
+    );
+  }
 }
 
 export default GraphBox;
