1e8d957d178336735985ad7f7f83ccac04b8d7c8
unknown <vkarinje@pdx.edu>
2022-03-08 15:44:16 -0800

    intendation errors

--- Bigboard/src/GraphBox.js
+++ Bigboard/src/GraphBox.js
@@ -1,231 +1,230 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
 import { Line } from "react-chartjs-2";
-// import Plot from 'react-plotly.js';
-import "./GraphBox.css";
-import moment from "moment";
-class GraphBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    Data:{}
-  };}
-  componentDidUpdate(pP) {
-    if (pP.currentStockCode !== this.props.currentStockCode) {
-      // console.log(this.props.currentStockCode);
-      // console.log(this.props.alter_Graph)
-      if (this.props.alter_Graph === false) {
-        let startDate = Math.round(new Date().getTime() / 1000);
-        let endDate = startDate - 72 * 3600;
-      } else {
-        let startDate = this.props.dates.endDate;
-        let endDate = this.props.dates.startDate;
-      }
-      const pointerToThis = this;
-      axios
-        .get("https://finnhub.io/api/v1/stock/candle", {
-          params: {
-            symbol: this.props.currentStockCode,
-            resolution: 5,
-            from: endDate,
-            to: startDate,
-            token: "bu5pnnf48v6qku34c7vg",
-          },
-        })
-        .then((response) => {
-          console.log(response);
-          // console.log({ JSON.stringify(new Date(response.data.t * 1000)).split('T')[0].replace('"', '') });
-          var unix_time = response.data.t;
-          var i;
-          var x = [];
-          // console.log(unix_time);
-          for (i = 0; i < unix_time.length; i++) {
-            var time = moment.unix(unix_time[i]).format("YYYY-MM-DD HH:mm");
-            x.push(time);
-          }
-          var y = response.data.c;
-          // console.log(typeof(y[0]));
-          var lowOrHighColor = y[0] < y[y.length - 1] ? "#81b737" : "#d54f4f";
-          this.setState({
-            symbol: this.props.currentStockCode,
-            text: "in the last 72 Hours",
-            Data: {
-              labels: x,
-              datasets: [
-                {
-                  label: "Stock Price",
-                  data: y,
-                  fill: true,
-                  lineTension: 0.1,
-                  backgroundColor: lowOrHighColor,
-                  borderColor: lowOrHighColor,
-                  borderCapStyle: "butt",
-                  borderJoinStyle: "miter",
-                  pointBorderColor: lowOrHighColor,
-                  pointBackgroundColor: lowOrHighColor,
-                  pointBorderWidth: 1,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: lowOrHighColor,
-                  pointHoverBorderColor: lowOrHighColor,
-                  pointHoverBorderWidth: 2,
-                  pointRadius: 2,
-                  pointHitRadius: 10,
-                },
-              ],
-            },
-          });
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
-    } else if (pP.dates !== this.props.dates) {
-      if (this.props.alter_Graph === false) {
-        var startDate = Math.round(new Date().getTime() / 1000);
-        var endDate = startDate - 72 * 3600;
-      } else {
-        var startDate = this.props.dates.endDate;
-        var endDate = this.props.dates.startDate;
-      }
-      const pointerToThis = this;
-      axios
-        .get("https://finnhub.io/api/v1/stock/candle", {
-          params: {
-            symbol: this.props.currentStockCode,
-            resolution: 5,
-            from: endDate,
-            to: startDate,
-            token: "bu5pnnf48v6qku34c7vg",
-          },
-        })
-        .then((response) => {
-          console.log(response.data);
-          if (response.data.s === "no_data") {
-            // const alert = useAlert();
-            alert(
-              "No Data Currently Available. Markets are closed during weekends and public holidays. Please filter by previous date."
-            );
-          } else {
-            var unix_time = response.data.t;
-            var i;
-            var x = [];
-            for (i = 0; i < unix_time.length; i++) {
-              var time = moment.unix(unix_time[i]).format("YYYY-MM-DD HH:mm");
-              x.push(time);
+import './GraphBox.css';
+import moment from 'moment';
+class GraphBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          Data:{}
+        };}
+    componentDidUpdate(pP){
+        if(pP.currentStockCode !== this.props.currentStockCode){
+            if (this.props.alter_Graph===false){
+              var startDate = Math.round(new Date().getTime() / 1000);
+              var endDate = startDate - (72 * 3600);
             }
-            var text =
-              "between " +
-              String(x[0]).slice(0, 10) +
-              " to " +
-              String(x[x.length - 1].slice(0, 10));
-            var y = response.data.c;
-            var lowOrHighColor = y[0] < y[y.length - 1] ? "#81b737" : "#d54f4f";
-            this.setState({
-              text: text,
-              symbol: this.props.currentStockCode,
-              Data: {
-                labels: x,
-                datasets: [
-                  {
-                    label: "Stock Price",
-                    data: y,
-                    fill: true,
-                    lineTension: 0.1,
-                    backgroundColor: lowOrHighColor,
-                    borderColor: lowOrHighColor,
-                    borderCapStyle: "butt",
-                    borderJoinStyle: "miter",
-                    pointBorderColor: lowOrHighColor,
-                    pointBackgroundColor: lowOrHighColor,
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: lowOrHighColor,
-                    pointHoverBorderColor: lowOrHighColor,
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 2,
-                    pointHitRadius: 10,
+            else{
+              var startDate = this.props.dates.endDate;
+              var endDate = this.props.dates.startDate;
+            }
+            const pointerToThis = this;
+            axios.get('https://finnhub.io/api/v1/stock/candle',{              
+            params:{
+                symbol: this.props.currentStockCode,
+                resolution: 5,
+                from: endDate,
+                to: startDate,
+                token: 'bu5pnnf48v6qku34c7vg'
+            }})
+            .then((response) => {
+              console.log(response);
+              var unix_time = response.data.t;
+              var i;
+              var x=[];
+                for (i = 0; i < unix_time.length; i++) {
+                  var time = moment.unix(unix_time[i]).format("YYYY-MM-DD HH:mm");
+                  x.push(time)
+                }
+              var y = response.data.c;
+              var lowOrHighColor = y[0] < y[y.length - 1] 
+                ? '#81b737' : '#d54f4f';
+              this.setState({
+                  symbol: this.props.currentStockCode,
+                  text: 'in the last 72 Hours',
+                  Data: {
+                    labels: x,
+                    datasets: [
+                      {
+                        label: "Stock Price",
+                        data: y,
+                        fill: true,
+                        lineTension: 0.1,
+                        backgroundColor: lowOrHighColor,
+                        borderColor: lowOrHighColor,
+                        borderCapStyle: "butt",
+                        borderJoinStyle: "miter",
+                        pointBorderColor: lowOrHighColor,
+                        pointBackgroundColor: lowOrHighColor,
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: lowOrHighColor,
+                        pointHoverBorderColor: lowOrHighColor,
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 2,
+                        pointHitRadius: 10,
+                      },
+                    ],
                   },
-                ],
-              },
+                });
+            })
+            .catch(error => {
+              console.log(error.response)
             });
+        }
+        else if(pP.dates!=this.props.dates){
+          if (this.props.alter_Graph===false){
+            var startDate = Math.round(new Date().getTime() / 1000);
+            var endDate = startDate - (72 * 3600);
+            
           }
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
-    }
-  }
-  render() {
-    return (
-      <div className="graphClass">
-        <h5>
-          Price per share of {this.state.symbol} {this.state.text}{" "}
-        </h5>
-        <Line
-          data={this.state.Data}
-          height={300}
-          width={400}
-          options={{
-            layout: {
-              padding: {
-                right: 20,
-              },
-            },
-            responsive: true,
-            maintainAspectRatio: false,
+          else{
+            var startDate = this.props.dates.endDate;
+            var endDate = this.props.dates.startDate;
+          }
+          const pointerToThis = this;
+        axios.get('https://finnhub.io/api/v1/stock/candle',{
+            
+          params:{
+              symbol: this.props.currentStockCode,
+              resolution: 5,
+              from: endDate,
+              to: startDate,
+              token: 'bu5pnnf48v6qku34c7vg'
 
-            scales: {
-              xAxes: [
-                {
-                  gridLines: {
-                    display: false,
-                  },
-                  ticks: {
-                    display: false, //this will remove only the label
-                  },
-                  scaleLabel: {
-                    display: true,
+          }
+        }
+        )
+          .then((response) => {
+            console.log(response.data);
+            if (response.data.s==="no_data")
+            {
+              alert('No Data Currently Available. Markets are closed during weekends and public holidays. Please filter by previous date.');
+            }
+            else{
+              var unix_time = response.data.t;
+              var i;
+              var x=[];
+              for (i = 0; i < unix_time.length; i++) 
+              {
+                var time = moment.unix(unix_time[i]).format("YYYY-MM-DD HH:mm");
+                x.push(time)
+              }
+              var text='between '+String(x[0]).slice(0,10)+' to '+String(x[x.length-1].slice(0,10))
+              var y = response.data.c;
+              var lowOrHighColor = y[0] < y[y.length - 1] 
+                ? 
+                '#81b737' : '#d54f4f';
+              this.setState({
+                text: text,
+                symbol: this.props.currentStockCode,
+                Data: {
+                  labels: x,
+                  datasets: 
+                  [
+                    {
+                      label: "Stock Price",
+                      data: y,
+                      fill: true,
+                      lineTension: 0.1,
+                      backgroundColor: lowOrHighColor,
+                      borderColor: lowOrHighColor,
+                      borderCapStyle: "butt",
+                      borderJoinStyle: "miter",
+                      pointBorderColor: lowOrHighColor,
+                      pointBackgroundColor: lowOrHighColor,
+                      pointBorderWidth: 1,
+                      pointHoverRadius: 5,
+                      pointHoverBackgroundColor: lowOrHighColor,
+                      pointHoverBorderColor: lowOrHighColor,
+                      pointHoverBorderWidth: 2,
+                      pointRadius: 2,
+                      pointHitRadius: 10,
+                    },
+                  ],
+                },
+              }
+              );
+            }
+          }
+          )
+          .catch(error => {
+            console.log(error.response)
+            }
+            );
+        }
 
-                    labelString: "Time",
-                    fontColor: "rgb(0, 35, 80)",
-                    fontfamily:
-                      "New Century Schoolbook, teX Gyre Schola, serif",
-                    fontsize: 12,
+      }
+
+    render(){
+        return(
+
+            <div className="graphClass">
+                <h5>Price per share of {this.state.symbol} {this.state.text} </h5>
+                <Line
+                data={this.state.Data}
+                height={300}
+                width ={400}
+                options={{
+                  layout:{
+                    padding:{
+                      right : 10 
+                    }
                   },
-                },
-              ],
-              yAxes: [
-                {
-                  gridLines: {
-                    display: false,
+                  responsive: true,
+                  maintainAspectRatio: false,
+    
+                  scales: {
+                    xAxes: [{
+                      gridLines: {
+                         display: false
+                      },
+                      ticks: {
+                        display: false //this will remove only the label
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: 'Time',
+                        fontColor: 'rgb(0, 35, 80)',
+                        fontfamily: 'New Century Schoolbook, TeX Gyre Schola, serif',
+                        fontSize: 12
+                      }
+                   }],
+                    yAxes: [{
+                      gridLines: {
+                         display: false,
+                         
+                      },
+                      scaleLabel: {
+                        display: true,
+                        labelString: 'Stock Price ($)',
+                        fontColor: 'rgb(0, 35, 80)',
+                        fontfamily: 'New Century Schoolbook, TeX Gyre Schola, serif',
+                        fontSize: 12
+                      },
+                      ticks: {
+                        display: true,
+                        fontColor: 'black'
+
+                   }
+                  }
+                ],
                   },
-                  scaleLabel: {
-                    display: true,
-                    labelString: "Stock Price ($)",
-                    fontColor: "rgb(0, 35, 80)",
-                    fontfamily:
-                      "New Century Schoolbook, teX Gyre Schola, serif",
-                    fontsize: 12,
+                  legend: {
+                      display: true,
+                      position: 'top',
+                    labels: {
+                      fontColor: 'rgb(0, 35, 80)',
+                      fontSize: 12,
+                      fontfamily: 'New Century Schoolbook, TeX Gyre Schola, serif',
+                    },
                   },
-                  ticks: {
-                    display: true,
-                    fontColor: "black",
-                  },
-                },
-              ],
-            },
-            legend: {
-              display: true,
-              position: "top",
-              labels: {
-                fontColor: "rgb(0, 35, 80)",
-                fontSize: 12,
-                fontfamily: "New Century Schoolbook, teX Gyre Schola, serif",
-              },
-            },
-          }}
-        />
-      </div>
-    );
-  }
+                }}
+              />
+            </div>
+
+        );
+    };
 }
 export default GraphBox;
--- Bigboard/src/InputBox.js
+++ Bigboard/src/InputBox.js
@@ -1,69 +1,69 @@
-import React from "react";
-import axios from "axios";
-import "./InputBox.css";
+import React from 'react'
+import axios from 'axios'
+import './InputBox.css'
 
 class InputBox extends React.Component {
   state = {
     inputArray: [],
-  };
+  }
 
   getResults = async (s, v) => {
-    console.log("123");
-    var stockcode;
+    console.log('123')
+    var stockcode
 
-    stockcode = document.getElementById("stockcode").value;
+    stockcode = document.getElementById('stockcode').value
 
-    let startDate = Math.round(new Date().getTime() / 1000);
-    let endDate = startDate - 72 * 3600;
+    let startDate = Math.round(new Date().getTime() / 1000)
+    let endDate = startDate - 72 * 3600
 
     if (this.state.inputArray.includes(stockcode)) {
-      alert("Already Exists");
-      document.getElementById("stockcode").value = "";
+      alert('Already Exists')
+      document.getElementById('stockcode').value = ''
     } else {
-      var val = document.getElementById("stockcode").value;
-      if (val === "" || val.length > 4) {
-        alert("Please enter correct code");
+      var val = document.getElementById('stockcode').value
+      if (val === '' || val.length > 4) {
+        alert('Please enter correct code')
       } else {
         var resp_data_table = await axios.get(
-          "https://finnhub.io/api/v1/quote",
+          'https://finnhub.io/api/v1/quote',
           {
             params: {
               symbol: stockcode,
-              token: "bu5pnnf48v6qku34c7vg",
+              token: 'bu5pnnf48v6qku34c7vg',
             },
-          }
-        );
+          },
+        )
 
         this.setState(
           { inputArray: this.state.inputArray.concat(stockcode) },
           () => {
             if (
-              resp_data_table.data.c == 0 &&
-              resp_data_table.data.h == 0 &&
-              resp_data_table.data.l == 0 &&
-              resp_data_table.data.o == 0 &&
-              resp_data_table.data.pc == 0 &&
-              resp_data_table.data.t == 0
+              resp_data_table.data.c === 0 &&
+              resp_data_table.data.h === 0 &&
+              resp_data_table.data.l === 0 &&
+              resp_data_table.data.o === 0 &&
+              resp_data_table.data.pc === 0 &&
+              resp_data_table.data.t === 0
             ) {
-              this.props.getGraphResults("no_data", "");
+              this.props.getGraphResults('no_data', '')
             } else {
-              resp_data_table.data.stockcode = stockcode;
+              resp_data_table.data.stockcode = stockcode
               this.props.getResults(true, {
                 stockcode: stockcode,
                 response: resp_data_table.data,
-              });
-              console.log(resp_data_table.data);
+              })
+              console.log(resp_data_table.data)
             }
-            document.getElementById("stockcode").value = "";
-          }
-        );
+            document.getElementById('stockcode').value = ''
+          },
+        )
       }
     }
-  };
+  }
 
   convertToUppercase() {
-    var val = document.getElementById("stockcode");
-    val.value = val.value.toUpperCase();
+    var val = document.getElementById('stockcode')
+    val.value = val.value.toUpperCase()
   }
   render() {
     return (
@@ -80,13 +80,13 @@ class InputBox extends React.Component {
           className="btn btn-dark inputbutton"
           aria-label="Search"
           type="submit"
-          onClick={() => this.getResults(true, "")}
+          onClick={() => this.getResults(true, '')}
         >
           <i className="fa fa-search"></i>
         </button>
       </div>
-    );
+    )
   }
 }
 
-export default InputBox;
+export default InputBox
